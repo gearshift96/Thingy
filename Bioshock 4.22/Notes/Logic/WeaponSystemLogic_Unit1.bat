3ee6843887d56dcaea2b317cbff0b42fa489a565
Danny <dannyjundi@gmail.com>
2019-09-08 17:43:15 -0700

    Unit Test File

---
+++ Bioshock 4.22/Notes/Logic/WeaponSystemLogic_Unit1.bat
@@ -0,0 +1,30 @@
+@echo off
+
+(
+echo hello
+echo there
+echo s
+echo d
+echo t
+echo r
+echo f
+echo 1
+echo f
+echo 3
+echo c
+echo 1
+echo c
+echo 3
+echo e
+echo f
+echo 1
+echo e
+echo s
+echo d
+echo t
+echo r
+echo s
+echo d
+echo t
+echo q
+) | WeaponSystemLogic.exe
